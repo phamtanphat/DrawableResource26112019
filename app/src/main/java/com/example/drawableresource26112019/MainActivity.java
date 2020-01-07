518751f52e25ecbfc7b9dcd3ec4c8a906ab97361
phatdroid94 <phatdroid94@gmail.com>
2020-01-07 18:11:07 +0700

    clip

--- app/src/main/java/com/example/drawableresource26112019/MainActivity.java
+++ app/src/main/java/com/example/drawableresource26112019/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.drawableresource26112019;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.ClipDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView mImgClip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImgClip = findViewById(R.id.imageClip);
+
+        final ClipDrawable clipDrawable = (ClipDrawable) mImgClip.getDrawable();
+
+        mImgClip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mImgClip.setImageLevel(clipDrawable.getLevel() + 1000);
+            }
+        });
+
     }
 }
